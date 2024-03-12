bab4c1fe1a4b2a395f082b4c2efcebad27d925c2
Bob Webb <bob.wbb@gmail.com>
2024-03-12 18:46:26 +0000

    remove output file

--- src/index.js
+++
@@ -1,7 +0,0 @@
-async function f() {
-    const c = await import("@canvas-js/chain-ethereum");
-    console.log(c.SIWESigner.name);
-    const j2 = await import("@ipld/dag-json");
-    console.log(j2.encode({ hello: "canvas" }));
-}
-f();
